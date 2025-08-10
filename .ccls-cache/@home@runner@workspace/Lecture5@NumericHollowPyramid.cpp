b8086d144ac828f8371bb54fc81c860b8543245e
sahil407x29 <alamsahil2907@gmail.com>
2025-08-10 10:52:24 +0000

    learnt right shift operator

---
+++ .ccls-cache/@home@runner@workspace/Lecture5@NumericHollowPyramid.cpp
@@ -0,0 +1,15 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int n;
+//   cout<<"enter no of times";
+//   cin>>n;
+
+//   for(int row=0;row<n;row++){
+//     for(int col=0;col<n-row-1;col++){
+//       cout<<"*";
+//     }
+    
+//     cout<<endl;
+//   }
+// }
