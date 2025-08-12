99e30103ccacf784734cef5b7b5affa4dd86512e
sahil407x29 <alamsahil2907@gmail.com>
2025-08-12 06:41:00 +0000

    learnt number system

--- Operators,Loops,Condtionals.cpp/main.cpp
+++ Operators,Loops,Condtionals.cpp/main.cpp
@@ -1,66 +1,66 @@
-#include <iostream>
-using namespace std;
-int main(){
+// #include <iostream>
+// using namespace std;
+// int main(){
 
 
-  // int a=-47;
-  // a = a>>1;
-  // cout<<a;
+//   // int a=-47;
+//   // a = a>>1;
+//   // cout<<a;
 
-  // pre / post
+//   // pre / post
 
-// int a=5;
-  // int sum;`
+// // int a=5;
+//   // int sum;`
   
-  // cout<<  (++a) * (++a);
+//   // cout<<  (++a) * (++a);
 
-  // for(int i=0;i<=5;i++){
-  //   if(i==1)
-  //     break;
-  //   cout << "sahil " ;
+//   // for(int i=0;i<=5;i++){
+//   //   if(i==1)
+//   //     break;
+//   //   cout << "sahil " ;
   
-  // }
-  // cout<<"alam";
-  // int value;
-  // cout<<"enter the value";
-  // cin>>value;
+//   // }
+//   // cout<<"alam";
+//   // int value;
+//   // cout<<"enter the value";
+//   // cin>>value;
 
-  // switch(value){
-  //   case 1: cout <<"sahil";
-  //   break;
+//   // switch(value){
+//   //   case 1: cout <<"sahil";
+//   //   break;
 
-  //   case 2: cout<<"alam";
-  //   break;
+//   //   case 2: cout<<"alam";
+//   //   break;
 
-  //   case 3: cout<<"tony";
-  //   break;
+//   //   case 3: cout<<"tony";
+//   //   break;
 
-  //   case 4: cout<<"ferguson";
-  //   break;
-  // switch(value){
-  //   case 1: cout <<"sahil";
-  //   break;
+//   //   case 4: cout<<"ferguson";
+//   //   break;
+//   // switch(value){
+//   //   case 1: cout <<"sahil";
+//   //   break;
 
-  //   case 2: cout<<"alam";
-  //   break;
+//   //   case 2: cout<<"alam";
+//   //   break;
 
-  //   case 3: cout<<"tony";
-  //   break;
+//   //   case 3: cout<<"tony";
+//   //   break;
 
-  //   case 4: cout<<"ferguson";
-  //   break;
-  // }
-  // // if the switch is not inside a loop, continue is illegal → compiler error.
-
-
+//   //   case 4: cout<<"ferguson";
+//   //   break;
+//   // }
+//   // // if the switch is not inside a loop, continue is illegal → compiler error.
 
 
-  // for(int i=0;i<=5;i++){
-  //   if(i>0)
-  //     continue;
-  //   cout << "sahil " ;
+
+
+//   // for(int i=0;i<=5;i++){
+//   //   if(i>0)
+//   //     continue;
+//   //   cout << "sahil " ;
     
-  // }
-  // cout<<"alam";
+//   // }
+//   // cout<<"alam";
   
-}
+// }
