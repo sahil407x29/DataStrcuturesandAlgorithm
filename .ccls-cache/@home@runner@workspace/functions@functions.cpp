847610ad4bdb14fa1787d054f6eb61baa2d58fe0
sahil407x29 <alamsahil2907@gmail.com>
2025-08-20 09:52:00 +0000

    learnt 2d arrays

---
+++ .ccls-cache/@home@runner@workspace/functions@functions.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+// using namespace std;
+// // int FindMax (int a,int b,int c){
+// // if(a>b && a>c){
+// //   return a;
+// // }
+// //   if(b>a && b>c){
+// //     return b;
+// //   }
+// //  else{
+// //    return c;
+// //  }
+  
+
+// // }
+
+// int EvenSum(int n){
+//   int sum =0;
+//   for(int i=0;i<=n;i+=2){
+//     // int sum =0;
+//     sum = sum + i;
+   
+//   } 
+//   return sum;
+// }
+// int main(){
+//   // int MaxNumber = FindMax(4,5,6);
+//   // cout<< MaxNumber;
+//   int counter = EvenSum(10);
+//   cout<< counter;
+// }
