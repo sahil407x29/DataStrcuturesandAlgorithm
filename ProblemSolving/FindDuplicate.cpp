638c86ed7a6acf7478e7e74070c2cdb637a34c0f
sahil407x29 <alamsahil2907@gmail.com>
2025-09-10 07:49:39 +0000

    problems solved

--- ProblemSolving/FindDuplicate.cpp
+++ ProblemSolving/FindDuplicate.cpp
@@ -1,43 +1,43 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-using namespace std;
-void findDuplicateMethod(vector<int> arr){
-  while(arr[0]!=arr[arr[0]]){
-    swap(arr[0],arr[arr[0]]);
-  }
-  cout<< arr[0];
-}
-void findDupicateVis(vector<int> arr){
-  int ans = 0;
-  for(int i = 0;i<arr.size();i++){
-    int index = abs(arr[i]);
+// #include <algorithm>
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// void findDuplicateMethod(vector<int> arr){
+//   while(arr[0]!=arr[arr[0]]){
+//     swap(arr[0],arr[arr[0]]);
+//   }
+//   cout<< arr[0];
+// }
+// void findDupicateVis(vector<int> arr){
+//   int ans = 0;
+//   for(int i = 0;i<arr.size();i++){
+//     int index = abs(arr[i]);
            
-     if(arr[index] <  0){
-       ans = index;
-       break;
-     }
-    else{
-      arr[index] *= -1;
-    }
-  } cout << ans;
-}
+//      if(arr[index] <  0){
+//        ans = index;
+//        break;
+//      }
+//     else{
+//       arr[index] *= -1;
+//     }
+//   } cout << ans;
+// }
 
-void findDuplicate(vector<int> arr) {
-  // this method works but modifies the original array 
-  sort(arr.begin(), arr.end()); 
+// void findDuplicate(vector<int> arr) {
+//   // this method works but modifies the original array 
+//   sort(arr.begin(), arr.end()); 
 
-  for (int i = 0; i < arr.size() - 1; i++) {
-    if (arr[i] == arr[i + 1]) {
-      cout << arr[i];
-      break;
-    }
-  }
-}
-int main() {
+//   for (int i = 0; i < arr.size() - 1; i++) {
+//     if (arr[i] == arr[i + 1]) {
+//       cout << arr[i];
+//       break;
+//     }
+//   }
+// }
+// int main() {
 
-  vector<int> arr = {1, 2, 3, 4, 4, 5};
-  // findDuplicate(arr);
-  // findDupicateVis(arr);
-  findDuplicateMethod(arr);
-}
+//   vector<int> arr = {1, 2, 3, 4, 4, 5};
+//   // findDuplicate(arr);
+//   // findDupicateVis(arr);
+//   findDuplicateMethod(arr);
+// }
---
+++ ProblemSolving/FindMissingElement.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+using namespace std;
+void findMissingSortSwap(vector<int> arr) {
+  while(int i=0 < garr.size()){
+   int index = arr[i] - 1;
+    if(arr[i]!=arr[index]){
+      swap(arr[i],arr[index]);
+    }
+    else {
+      i++;
+    }
+ }
+  for(int i=0;i<arr.size();i++){
+   if(arr[i]!=i+1){
+     cout<< i+1;
+   }
+  }
+    
+}
+void FindMissing(vector<int> arr){
+  for(int i=0;i<arr.size();i++){
+    int index = abs(arr[i]);
+    if(arr[index-1]>0){
+      arr[index-1] *=-1;
+      // index-1 → because numbers are 1-based, but array positions are 0-based.
+    }
+  }
+  for(int i=0;i<arr.size();i++){
+    if(arr[i] > 0){
+      cout << i+1 << " ";
+      // i+1 → because when checking positions, index i corresponds to number (i+1).
+    }
+  }
+}
+
+int main(){
+  vector<int> arr= {1,3,4,4,3};
+  FindMissing(arr);
+}
